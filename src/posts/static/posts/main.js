80422f74c9d287cabc068bfa17567eac5083bb77
Sungsoon Park <sungsoon.park@gmail.com>
2021-03-13 16:45:35 -0800

    signals, views, urls, templates, javascripts and first DOM manipulations done

---
+++ src/posts/static/posts/main.js
@@ -0,0 +1,6 @@
+console.log('hello World!')
+
+const helloWorld = document.getElementById('hello-world')
+
+// helloWorld.textContent = "Mochi Muffins"
+helloWorld.innerHTML = "Mochi <b>Muffins</b>"
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def post_list_and_create(request):
+    qs = Post.objects.all()
+    context = {
+        'qs': qs
+    }
+    return render(request, 'posts/main.html', context)
